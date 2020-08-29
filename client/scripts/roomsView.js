e00861b9ba4c6f115e99848b7018c4dc7d0f67ea
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-08-28 23:05:18 -0700

    Allow adding rooms to the DOM

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -1,14 +1,47 @@
 var RoomsView = {
 
-  $button: $('#rooms button'),
-  $select: $('#rooms select'),
+  $room: $('#rooms .room'),
+  $select: $('select'),
+  $addroom: $('.room'),
 
   initialize: function() {
+    RoomsView.$addroom.on('submit', RoomsView.addRoom);
   },
 
   render: function() {
+    //
+  },
+
+  renderRoom: function(roomname) {
+    // set var optiontag,
+    var optionTag = `<option value="${roomname}">${roomname}</option>`;
+
+    $('select').append(optionTag);
+  },
+
+  //
+  addRoom: function(roomname) {
+    //
   }
-
-
-
 };
+
+
+// handleSubmit: function(event) {
+//   // Stop the browser from submitting the form
+//   event.preventDefault();
+
+//   // user text submetted
+//   var message = {
+//     username: window.location.search.substring(10),
+//     text: $('#message').val(),
+//     roomname: 'testing'
+//   };
+
+//   // send msg in server(POST)
+//   Parse.create(message);
+
+//   console.log('submit click!');
+
+//   // receive the updated data
+//   App.fetch();
+// },
